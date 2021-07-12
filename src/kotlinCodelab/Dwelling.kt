596da6cd3641651676a51383086006a39757ed0f
Augustine Victor <augustinevickky@gmail.com>
2021-07-12 09:47:16 +0100

    init

--- src/kotlinCodelab/Dwelling.kt
+++ src/kotlinCodelab/Dwelling.kt
@@ -11,6 +11,16 @@ abstract class Dwelling(private var residents:Int){
 class SquareCabin(residents: Int):Dwelling(residents){
     override val buildingMaterial: String ="Wood"
     override val capacity: Int=6
+}
 
+ open class RoundHut(residents: Int):Dwelling(residents){
+    override val buildingMaterial: String
+        get() = "Straw"
+    override val capacity: Int
+        get() = 4
+}
 
+class RoundTower(residents: Int):RoundHut(residents){
+    override val buildingMaterial: String ="Stone"
+    override val capacity: Int=5
 }
--- src/kotlinCodelab/Main.kt
+++ src/kotlinCodelab/Main.kt
@@ -8,4 +8,18 @@ fun main(args: Array<String>) {
         println("Material: $buildingMaterial")
         println("Has room? ${hasRoom()}")
     }
+    val roundHut=RoundHut(8)
+    with(roundHut){
+        println("\nRound Hut\n============")
+        println("Capacity: $capacity")
+        println("Material: $buildingMaterial")
+        println("Has room? ${hasRoom()}")
+    }
+    val roundTower=RoundTower(4)
+    with(roundTower){
+        println("\nRound Tower\n============")
+        println("Capacity: $capacity")
+        println("Material: $buildingMaterial")
+        println("Has room? ${hasRoom()}")
+    }
 }
