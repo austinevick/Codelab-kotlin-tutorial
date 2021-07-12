d055fe20ba8493910869470054bd20393346e44b
Augustine Victor <augustinevickky@gmail.com>
2021-07-12 22:53:23 +0100

    init

--- src/kotlinCodelab/Dwelling.kt
+++ src/kotlinCodelab/Dwelling.kt
@@ -1,26 +1,37 @@
 package kotlinCodelab
 
-abstract class Dwelling(private var residents:Int){
-    abstract val buildingMaterial: String
-    abstract val capacity:Int
+import kotlin.math.PI
 
-    fun hasRoom():Boolean{
-        return residents<capacity
+abstract class Dwelling(private var residents: Int) {
+    abstract val buildingMaterial: String
+    abstract val capacity: Int
+    abstract fun floorArea(): Double
+    fun hasRoom(): Boolean {
+        return residents < capacity
     }
 }
-class SquareCabin(residents: Int):Dwelling(residents){
-    override val buildingMaterial: String ="Wood"
-    override val capacity: Int=6
+
+class SquareCabin(residents: Int, private val length: Double) : Dwelling(residents) {
+    override val buildingMaterial: String = "Wood"
+    override val capacity: Int = 6
+    override fun floorArea(): Double {
+        return length * length
+    }
 }
 
- open class RoundHut(residents: Int):Dwelling(residents){
+open class RoundHut(residents: Int, private val radius: Double) : Dwelling(residents) {
     override val buildingMaterial: String
         get() = "Straw"
     override val capacity: Int
         get() = 4
+
+    override fun floorArea(): Double {
+        return PI * radius * radius
+    }
 }
 
-class RoundTower(residents: Int):RoundHut(residents){
-    override val buildingMaterial: String ="Stone"
-    override val capacity: Int=5
+class RoundTower(residents: Int, radius: Double, floors: Int = 2)
+    : RoundHut(residents, radius) {
+    override val buildingMaterial: String = "Stone"
+    override val capacity: Int = 5 * floors
 }
--- src/kotlinCodelab/Main.kt
+++ src/kotlinCodelab/Main.kt
@@ -1,21 +1,22 @@
 package kotlinCodelab
 
 fun main(args: Array<String>) {
-    var squareCabin=SquareCabin(4)
+    var squareCabin=SquareCabin(4,50.0)
     with(squareCabin) {
         println("\nSquare Cabin\n============")
         println("Capacity: $capacity")
         println("Material: $buildingMaterial")
         println("Has room? ${hasRoom()}")
+        println("Floor area: ${floorArea()}")
     }
-    val roundHut=RoundHut(8)
+    val roundHut=RoundHut(8,10.0)
     with(roundHut){
         println("\nRound Hut\n============")
         println("Capacity: $capacity")
         println("Material: $buildingMaterial")
         println("Has room? ${hasRoom()}")
     }
-    val roundTower=RoundTower(4)
+    val roundTower=RoundTower(4,15.5)
     with(roundTower){
         println("\nRound Tower\n============")
         println("Capacity: $capacity")
